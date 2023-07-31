bf7720cc7fb92c0a78204c5fe9b9403b3d7839d4
anthonytan26 <anthonytan8u8u@gmail.com>
2023-07-31 21:34:14 +0800

    Completed Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse_string = "";
+    let length = string.length
+    for (let i = length; i > 0; i--) {
+      reverse_string += string[i-1];
+    }
+    return reverse_string;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
