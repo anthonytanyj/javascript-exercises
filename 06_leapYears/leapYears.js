e5484c6c0fe3b4b68cd32a195204585d813ff6e7
anthonytan26 <anthonytan8u8u@gmail.com>
2023-08-01 09:19:05 +0800

    Complete Exercise 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let yearInt = parseInt(year)
 
+    if (yearInt % 4 === 0 && (yearInt % 100 != 0 || yearInt % 400 ===0)) {
+        return true
+    }
+    else {
+        return false
+    }
+    
 };
 
 // Do not edit below this line
