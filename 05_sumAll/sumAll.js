31a1a14f8236a9ac26c9692c4ad6bd2270656197
anthonytan26 <anthonytan8u8u@gmail.com>
2023-08-01 08:58:17 +0800

    Complete Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    let firstInteger = parseInt(firstNumber);
+    let secondInteger = parseInt(secondNumber);
+    let finalSum = 0
 
+    for (i=firstInteger; i <= secondInteger; i++) {
+        finalSum += i
+    }
+    return finalSum
 };
 
 // Do not edit below this line
