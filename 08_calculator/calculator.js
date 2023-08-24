fa802efbe87ce3376ac2910e159b4db9b8210ebb
anthonytan26 <anthonytan8u8u@gmail.com>
2023-08-24 21:48:07 +0800

    Complete Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  array.forEach(num =>
+    sum += num)
+  return  sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	let multiply = 1;
+  array.forEach(num =>
+    multiply *= num)
+  return  multiply;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(a) {
+	if (a < 0){
+    console.log('Please enter positive number') 
+  } else if (a === 0) {
+    return 1
+  } else {
+    let fact = 1;
+    for (i=1;i<=a;i++){
+      fact *= i
+    }
+    return fact
+  }
+  }
 
 // Do not edit below this line
 module.exports = {
