5cf1159d5eb72d57a62c0c9477bc07e5a42a9c67
anthonytan26 <anthonytan8u8u@gmail.com>
2023-08-25 09:04:17 +0800

    Complete exercise 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let cleanWord = word.toString().replace(/[^\w\s\']|_/g, "")
+    .replace(/\s/g, "").toLowerCase();
 
+    let new_word = ""
+    let word_length = cleanWord.length
+    for(i = word_length-1; i >= 0; i--){
+        new_word+=cleanWord.charAt(i)
+    }
+    return cleanWord === new_word;
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
