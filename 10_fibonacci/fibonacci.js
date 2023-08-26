a2422415f7d20e40c97a2c945f1ec3dde45832a3
anthonytan26 <anthonytan8u8u@gmail.com>
2023-08-26 16:41:38 +0800

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(num) {
+    let numCleaned = parseInt(num);
+    let a = 0;
+    let b = 1;
+    if (numCleaned <= 0) {
+        return "OOPS"
+    } else if (numCleaned === 1){
+        return 1
+    } else {
+        for (let i=1;i<=numCleaned;i++){
+            let temp = a;
+            a = b;
+            b = temp + b;
+            
+        }
+        return a
+    }
+    };
 
 // Do not edit below this line
 module.exports = fibonacci;
